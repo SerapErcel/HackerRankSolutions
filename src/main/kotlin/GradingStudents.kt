6b6d9c9c39957da296de4fddfa8cc0dc7897eb5b
Serap <ercelserap@gmail.com>
2023-06-01 21:12:35 +0300

    Add Grading Students

---
+++ src/main/kotlin/GradingStudents.kt
@@ -0,0 +1,52 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fun gradingStudents(grades: Array<Int>): Array<Int> {
+    // Write your code here
+    val arr = grades.map {
+        when {
+            it < 38 -> it
+            it % 5 == 4 -> it + 1
+            it % 5 == 3 -> it + 2
+            else -> it
+        }
+    }
+    return arr.toTypedArray()
+}
+
+fun main(args: Array<String>) {
+    val gradesCount = readLine()!!.trim().toInt()
+
+    val grades = Array<Int>(gradesCount, { 0 })
+    for (i in 0 until gradesCount) {
+        val gradesItem = readLine()!!.trim().toInt()
+        grades[i] = gradesItem
+    }
+
+    val result = gradingStudents(grades)
+
+    println(result.joinToString("\n"))
+}
